6e2d02fb16a8872745a26bdbc0c1ee3fb2c8ef16
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-17 22:13:25 -0300

    configuring mongoose errors messages

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -8,7 +8,7 @@ const creditSchema = new mongoose.Schema({
 
 const debitSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito'] },
     status: { type: String, required: false, uppercase: true, enum: ['PAID', 'PENDING', 'SCHEDULED'] }
 })
 
@@ -21,3 +21,21 @@ const billingCycleSchema = new mongoose.Schema({
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
+
+/*
+
+ex test post (x-www-form-urlcoded) :
+
+name: Jan/20
+month: 1
+year: 2020
+credits[0][name]: Company
+credits[0][value]: 6500
+credits[1][name]: Professor
+credits[1][value]: 1500
+debits[0][name]: Telephone
+debits[0][value]: 89.20
+debits[0][status]: PAID
+
+
+*/
--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+// translate msg error
+mongoose.Error.messages.general.required = "Attribute '{PATH}' is required."
+mongoose.Error.messages.Number.min = "'{VALUE}' is less than '{MIN}'."
+mongoose.Error.messages.Number.max = "'{VALUE}' is greater than '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' is not valid for attribute '{PATH}'."
