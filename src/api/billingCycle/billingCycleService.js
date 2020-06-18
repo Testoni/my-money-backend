5b2a06c9bf4759c3b7181847e1a230f9f0385658
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-17 22:21:52 -0300

    count rest of billing cycles

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,14 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 // always return new object updated
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if (error) {
+            res.status(500).json({error: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle
