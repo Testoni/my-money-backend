36e99f565862ed01d3d3f118e306ae3d2147201d
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-18 00:13:44 -0300

    enabling cors

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,12 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 // all requests use bodyparser in url encoded format
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`Running on port ${port}`)
