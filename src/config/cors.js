5f5aaf3ab15f13ab383583de5a05364afecbf6bf
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-01 17:02:51 -0300

    configure auth to validate token

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     res.header('Acces-Control-Allow-Origin', '*')
     res.header('Acces-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Acces-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
+    res.header('Acces-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization')
     next()
 }
