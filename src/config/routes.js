592b92da478452cdbcb737de64e4f62aaff0c1c8
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-01 17:08:56 -0300

    configuring protected routes by jwt and open routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,36 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
+
+    /**
+     * Protected routes by JWT Token
+     */    
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /**
+     * Open routes
+     */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+    // OLD
     // define URL all routes
-    const router = express.Router()
-    server.use('/api', router)
+    // const router = express.Router()
+    // server.use('/api', router)
 
     // Routes Payment Cycle
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    // const BillingCycle = require('../api/billingCycle/billingCycleService')
+    // BillingCycle.register(router, '/billingCycles')
 }
